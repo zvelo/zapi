b3e69014ac1337b7ff743af1dc8cfcf59cab520d
Joshua Rubin <jrubin@zvelo.com>
2017-11-13 15:29:56 -0700

    update version to 1.3.3
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ var (
 	tlsInsecureSkipVerify  bool
 	mockNoCredentials      bool
 
-	version         = "v1.3.2"
+	version         = "v1.3.3"
 	app             = cli.NewApp()
 	defaultScopes   = strings.Fields(zapi.DefaultScopes)
 	defaultDatasets = []string{msg.CATEGORIZATION.String()}
